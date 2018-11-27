1e454784383302225b1eb6d461a9d9c6daa5b64b
Olamide Akomolafe <olamideakomolafe1234@gmail.com>
2018-11-27 15:15:03 +0000

    made a change to the contact page.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,18 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent  {
-  email: string;
-  firstname: string;
-  lastname: string;
-  message: string;
-  subject: string;
-  send() {
-    console.log(this.subject);
-  }
-
 }
